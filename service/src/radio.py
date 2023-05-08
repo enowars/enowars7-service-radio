60cc205e3e9a2545c8f50996cae0726d2ad5747c
tomerhawk <t.yavor@gmx.net>
2023-05-08 17:08:40 +0200

    radio player

--- service/src/radio.py
+++ service/src/radio.py
@@ -1,41 +1,42 @@
-import pyaudio
-import requests
-import sqlite3
+import vlc
 
-conn = sqlite3.connect()
-cursor = conn.cursor()
+# Create instance of VLC player
+instance = vlc.Instance("--no-xlib")
 
-cursor.execute("CREATE TABLE IF NOT EXISTS ")
+# Create new Media Player object
+player = instance.media_player_new()
 
-stream_url = "https://streams.kqed.org/kqedradio"  # replace with the actual stream URL
+# Define list of radio station URLs and names TODO add some more
+radio_stations = [
+    {
+        "name": "News",
+        "url": "http://kcrw.streamguys1.com/kcrw_192k_mp3_on_air_internet_radio",
+    },
+    {"name": "Something", "url": "http://strm112.1.fm/acountry_mobile_mp3"},
+    {
+        "name": "Techno",
+        "url": "https://regiocast.streamabc.net/regc-90s90stechno2195701-mp3-192-2408420",
+    },
+    {"name": "Dutch", "url": "http://icecast.omroep.nl/3fm-bb-mp3"},
+    {
+        "name": "Pokemon",
+        "url": "https://d3ctxlq1ktw2nl.cloudfront.net/staging/2021-02-22/076cb4bb38331a52417f0317d1ef07a9.m4a",
+    },
+]
 
-chunk_size = 1024  # number of bytes to read at a time
-audio_format = pyaudio.paInt16  # audio format (16-bit signed integer)
-channels = 2  # stereo
-sample_rate = 44100  # sample rate (Hz)
+# Display list of radio stations and prompt user to select one
+print("Choose a radio station:")
+for i, station in enumerate(radio_stations):
+    print(f'{i + 1}. {station["name"]}')
+selection = int(input()) - 1
 
-p = pyaudio.PyAudio()
-stream = requests.get(stream_url, stream=True)
+# Set selected radio station as the media to be played by the player
+media = instance.media_new(radio_stations[selection]["url"])
+player.set_media(media)
 
+# Start playback
+player.play()
 
-def callback(in_data, frame_count, time_info, status):
-    data = stream.read(chunk_size)
-    return (data, pyaudio.paContinue)
-
-
-stream = p.open(
-    format=audio_format,
-    channels=channels,
-    rate=sample_rate,
-    output=True,
-    stream_callback=callback,
-)
-
-stream.start_stream()
-
-while stream.is_active():
+# Wait for playback to finish
+while True:
     pass
-
-stream.stop_stream()
-stream.close()
-p.terminate()
