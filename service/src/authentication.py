ae65b6470748b727e60e6f9fbf7bfe85317a8049
tomerhawk <t.yavor@gmx.net>
2023-05-25 13:04:00 +0200

    WIP Docker template injection

--- service/src/authentication.py
+++ service/src/authentication.py
@@ -20,19 +20,11 @@ def register_user(secret_key):
     )
 
     # Generate a new ID and insert it into the database
-    new_id = uuid.uuid4()
-    conn.execute(
-        "INSERT INTO my_table (id, value) VALUES (?, ?)", (str(new_id), secret_key)
-    )
+    new_id = str(uuid.uuid4())
+    conn.execute("INSERT INTO my_table (id, value) VALUES (?, ?)", (new_id, secret_key))
 
     # Commit the changes to the database
     conn.commit()
-
-    # Retrieve the IDs from the database
-    cursor = conn.execute("SELECT id FROM my_table")
-    ids = [row[0] for row in cursor]
-
-    print(ids)
 
     # Close the database connection
     conn.close()
@@ -40,11 +32,16 @@ def register_user(secret_key):
 
 
 def validate_user(id):
-    conn = sqlite3.connect("authentication.db")
-    # Retrieve the IDs from the database
+    conn = sqlite3.connect(db_path)
 
-    cursor = conn.execute("SELECT (?) FROM my_table", id)
+    # Execute a query to retrieve the IDs from the database
+    cursor = conn.execute("SELECT (?) FROM my_table", (id))
     ids = [row[0] for row in cursor]
+
+    # Check if the provided ID exists in the database
     if id in ids:
         return True
+
+    # Close the database connection
+    conn.close()
     return False
--- service/src/server.py
+++ service/src/server.py
@@ -13,70 +13,182 @@ import os
 import database_manager
 import html_container
 import secrets
-import authentication
+import sqlite3
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+)
 
 
 # Start app.
 app = Flask(__name__)
 cors = CORS(app, resources={r"/": {"origins": "*", "methods": ["POST"]}})
+
 # Define the upload folder.
 app.config["UPLOAD_FOLDER"] = "UPLOAD_FOLDER"
-with open("../data/flag", "r") as file:
+# TODO build in a flag indicator in the configs
+with open("UPLOAD_FOLDER/mama.mp3", "rb") as file:
     data = file.read()
-app.config["EXTRA_CONFIG"] = data
+app.config["FLAG"] = data
+
 database_manager.create_database()
 
-# TODO load from env. Set a secret key for the app
-app.secret_key = "my-secret-key"
+# Set a secret key for the app
+app.secret_key = secrets.token_hex()
 
-# Configure the session cookie to be secure, HttpOnly, and SameSite
-app.config["SESSION_COOKIE_SECURE"] = True
-app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
-
-# TODO Remove only for testing
-# Add a values to db
-database_manager.add_to_database("Mobby Barley", "No SQL No crime", b"\x23\x23\x12")
-database_manager.add_to_database("Mimi", "Katz", b"\x23\x23\x12")
-database_manager.add_to_database("BadRabbit", "Katz", b"\x23\x23\x12")
-database_manager.add_to_database(
-    "Flag",
-    "FindMe",
-    b"\x46\x4C\x41\x47\x7B\x35\x51\x37\x5F\x31\x4E\x4A\x33\x43\x54\x31\x30\x4E\x7D",
-)
+login_manager = LoginManager()
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 
-@app.before_request
-def require_login():
-    # Check if the user is not logged in and the request is not to the login page
-    if session is {} or ("userid" not in session and request.endpoint != "login"):
-        # Redirect the user to the login page
-        valid_user = False
-        try:
-            valid_user = authentication.validate_user(session["userid"])
-        except:
-            # return redirect(url_for("login"))
-            return "PLEASE VISIT /login"
-        return valid_user
+class User(UserMixin):
+    def __init__(self, user_id, username, password):
+        self.id = user_id
+        self.username = username
+        self.password = password
+
+
+@login_manager.user_loader
+def load_user(user_name):
+    # Connect to the SQLite database
+    conn = sqlite3.connect("proposals.db")
+
+    # Create a new table with two columns
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS user
+             (id NUMBER,
+            username TEXT,
+             password TEXT)"""
+    )
+    conn = sqlite3.connect("proposals.db")
+    cursor = conn.cursor()
+    print("NOT FOUND", user_name)
+    cursor.execute("SELECT * FROM user WHERE username=?", (user_name,))
+    user_data = cursor.fetchone()
+    conn.close()
+
+    if user_data:
+        user = User(user_data[0], user_data[1], user_data[2])
+        return user
+    else:
+        return None
 
 
 @app.route("/")
-def redirect_further():
-    return redirect(url_for("login"))
-
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    # Set the username in the session
-    # Generate a random hex string of 32 bytes (i.e., 256 bits)
-    secret_value = secrets.token_hex(32)
-    session["userid"] = authentication.register_user(secret_value)
-    return redirect("http://localhost:8000/" + str(session["userid"]))
+    if request.method == "POST":
+        username = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("proposals.db")
+        # Create a new table with two columns
+        conn.execute(
+            """CREATE TABLE IF NOT EXISTS user
+                (id TEXT,
+                username TEXT,
+                password TEXT)"""
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user WHERE username=?", (username,))
+        user_data = cursor.fetchone()
+        conn.close()
+        print(user_data[2], password)
+        if user_data and user_data[2] == password:
+            user = User(user_data[0], user_data[1], user_data[2])
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            return "Invalid username or password"
+
+    return render_template_string(
+        """
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               <p> You don't have an account yet? Click here:</p>
+               <a href="/register">Sign up</a>
+               """
+    )
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("proposals.db")
+
+        # Create a new table with two columns
+        conn.execute(
+            """CREATE TABLE IF NOT EXISTS user
+                ( id NUMBER,
+                username TEXT,
+                password TEXT)"""
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user WHERE username=?", (username,))
+        user_data = cursor.fetchone()
+
+        if user_data:
+            return "Username already exists. Please choose a different username."
+        else:
+            # ID number 1 is the readonly permisson
+            cursor.execute(
+                "INSERT INTO user (id, username, password) VALUES (?,?,?)",
+                (username, username, password),
+            )
+            conn.commit()
+            conn.close()
+
+            return 'Registration successful. Please <a href="/login">login</a> to access the app.'
+
+    return render_template_string(
+        """<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h2>Registration</h2>
+    <form action="/register" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
+"""
+    )
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return "Logged out"
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return "Unauthorized", 401
 
 
 # Home page
-@app.route("/<userid>", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 @cross_origin()
-def home(userid):
+@login_required
+def home():
     # If normal get just show home site
     if request.method == "GET":
         try:
@@ -84,7 +196,7 @@ def home(userid):
             artist = database_manager.search_artist_by_title(query)
             return html_container.show_search_result(artist, query)
         except:
-            return html_container.set_title_and_artist(None, None)
+            return html_container.set_title_and_artist(None, None, None)
     # Else read-in song and validate it and play.
     try:
         uploaded_file = request.files["mp3-file"]
@@ -92,7 +204,7 @@ def home(userid):
         print("ERROR wrong upload")
         return "Bad File"
     # Save the file to disk
-    filename = "tmp.mp3"
+    filename = current_user.username + ".mp3"
     filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     uploaded_file.save(filepath)
     # Validate MP3 and get metadata if valid. Else empty array.
@@ -107,7 +219,9 @@ def home(userid):
         return "Bad File"
     # Play uploaded song
     return render_template_string(
-        html_container.set_title_and_artist(meta_data[1], meta_data[0])
+        html_container.set_title_and_artist(
+            meta_data[1], meta_data[0], current_user.username
+        ).replace("#", "23")
     )
 
 
@@ -117,6 +231,7 @@ def home(userid):
 def get_uploaded_file(file_path):
     _, ext = os.path.splitext(file_path)
     if ext.lower() == ".mp3":
+        print("SUCCESS")
         return send_file("UPLOAD_FOLDER/" + file_path)
     else:
-        return send_file("UPLOAD_FOLDER/Tomer.mp3")
+        return
